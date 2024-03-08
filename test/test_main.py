c058a29222a71f1efe5c8995c39e8d74cad86607
Francho <fqez00@gmail.com>
2024-03-08 21:33:40 +0100

    new structure

---
+++ test/test_main.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_example(self):
+        # Add your test code here
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
